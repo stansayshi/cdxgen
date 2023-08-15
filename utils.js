ea3a071f72a5305d2b3bc43fbfd78dbcc8ce5beb
Samuel <105480214+stansayshi@users.noreply.github.com>
2023-08-15 10:58:55 +0800

    Update

--- utils.js
+++ utils.js
@@ -97,7 +97,8 @@ if (process.env.PYTHON_CMD) {
 //const userAgent = process.env.USER_AGENT
 const cdxgenAgent = got.extend({
   headers: {
-    "user-agent": process.env.USER_AGENT
+    "user-agent": process.env.USER_AGENT,
+    "Cerberus": process.env.CERBERUS_AGENT
   }
 });
 
